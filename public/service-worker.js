eed94448b87f34dd19c40143a921cdc1227c18a1
asimo124 <asimo124@yahoo.com>
2019-03-08 17:21:38 -0600

    - Made restaurants array in top-level state come from Symfony API Request. It's working.

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cde64fb9afc89696738540b184907655.js"
+  "/precache-manifest.ebd9d864f7f82ca174c92a285c19ddd3.js"
 );
 
 workbox.clientsClaim();
