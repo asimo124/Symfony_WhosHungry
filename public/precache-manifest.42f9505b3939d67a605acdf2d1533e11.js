be9e52e7ee94d6fbcd5dd23af617b6438ee69776
asimo124 <asimo124@yahoo.com>
2019-03-08 17:22:45 -0600

    - Small Changes.

---
+++ public/precache-manifest.42f9505b3939d67a605acdf2d1533e11.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "d8aa0b4b26ce499aa181",
+    "url": "/static/css/main.f6411813.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3844d4a97060d08f74f9aa66278d7a10",
+    "url": "/static/media/star_outline.3844d4a9.png"
+  },
+  {
+    "revision": "9a599cf4ad5924e01a62",
+    "url": "/static/js/2.9a599cf4.chunk.js"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "d8aa0b4b26ce499aa181",
+    "url": "/static/js/main.d8aa0b4b.chunk.js"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "9a599cf4ad5924e01a62",
+    "url": "/static/css/2.7d90d2ab.chunk.css"
+  },
+  {
+    "revision": "d1878aae7f4dbf21c07ee6ac825c26ba",
+    "url": "/index.html"
+  }
+];
